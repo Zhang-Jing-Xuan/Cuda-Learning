74b6dd85ee84b172095dd8e4d276c046d2fa4c7c
Zhang-Jing-Xuan <zjx617@yeah.net>
2021-04-07 22:09:38 +0800

    Upload from mistgpu

---
+++ basis/main.cu
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <time.h>
+
+__global__ void add(int* a, int* b, int* c, int num)
+{
+	int i = threadIdx.x; //the id of thread
+	if(i < num)
+	{
+		c[i] = a[i] + b[i];
+	}
+}
+
+int main(void)
+{
+	// init data
+	int num = 10;
+	int a[num], b[num], c[num];
+	int *a_gpu, *b_gpu, *c_gpu;
+
+	for(int i = 0; i < num; i++)
+	{
+		a[i] = i;
+		b[i] = i * i;
+	}
+
+	cudaMalloc((void **)&a_gpu, num * sizeof(int));
+	cudaMalloc((void **)&b_gpu, num * sizeof(int));
+	cudaMalloc((void **)&c_gpu, num * sizeof(int));
+
+	// copy data
+	cudaMemcpy(a_gpu, a, num * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(b_gpu, b, num * sizeof(int), cudaMemcpyHostToDevice);
+
+	//do 
+	add <<<1,num>>>(a_gpu,b_gpu,c_gpu,num);// 1 block,10 thread
+	
+	// get data
+	cudaMemcpy(c, c_gpu, num * sizeof(int), cudaMemcpyDeviceToHost);
+
+	// visualization
+	for(int i = 0; i < num; i++)
+	{
+		printf("%d + %d = %d\n", a[i], b[i], c[i]);
+	}
+
+	return 0;
+}
